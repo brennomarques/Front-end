cc3df7d3ae05bab30746bd966c8ad592242aa301
brenno <brenno.marquesb@gmail.com>
2019-08-22 18:57:45 -0300

    cadastro

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,84 @@
 <template>
   <div id="app">
-
-    <nav>
-      <div class="nav-wrapper blue darken-1">
-        <a href="#" class="brand-logo center">System user</a>
-      </div>
-    </nav>
-
     <div class="container">
-      <div class="campos">
-        <form>
+      <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">Cadastro de Usuário</a>
+      </div>
+      </nav>
+  
+      <div class="row">
+        <div class="col s4">
+      <form>
+        <div class="campos">
+          <label>Nome</label>
+          <input type="text" placeholder="Nome completo">
 
-            <label>Nome</label>
-            <input type="text" placeholder="Nome completo">
+          <label>CPF</label>
+          <input type="number" placeholder="Cpf">
 
-            <label>CPF</label>
-            <input type="number" placeholder="Cpf">
+          <label>Telefone</label>
+          <input type="text" placeholder="Telefone">
 
-            <label>Telefone</label>
-            <input type="text" placeholder="Telefone">
+          <label>E-mail</label>
+          <input type="text" placeholder="E-mail@email.com">
 
-            <label>E-mail</label>
-            <input type="text" placeholder="E-mail@email.com">
+          <label>Data Nascimento</label>
+          <input type="Date" placeholder="00/00/0000">
 
-            <label>Data Nascimento</label>
-            <input type="Date" placeholder="00/00/0000">
+          <label>ID Endereço</label>          
+          <input type="number" placeholder="Informa o ID Endereço ou Cadastra"/>
+                    
+          <label>Senha</label>
+          <input type="Password" placeholder="Senha">
+          <br>
+          <div class="card-action">
+            <button class="waves-effect waves-light btn pulse">Gravar<i class="material-icons right">send</i></button>  
+          </div>
+        </div>
+      </form>
+        </div>
+        <div class="col s8">
+      <form>
+        <div class="tabela elementExample_first">
+          <table>
+            <thead>
 
-            <div id="area-form">
-              <label>ID Endereço</label>
-              <input type="number" placeholder="Informa o ID Endereço ou Cadastra"/>
-              <button class="waves-effect waves-light btn">Salvar<i class="material-icons left">cloud</i></button>
-            </div>
-            
-            <label>Senha</label>
-            <input type="Password" placeholder="Senha">
-          
-        </form>
-        <br>
-        <div class="card-action">
-          <button class="waves-effect waves-light btn pulse">Salvar<i class="material-icons left">cloud</i></button>  
+              <tr>
+                <th colspan="1">#ID</th>
+                <th colspan="4">NOME</th>
+                <th colspan="2">CPF</th>
+                <th colspan="0">E-mail</th>
+                <th colspan="2">Telefone</th>
+                <th colspan="2">Data Nascimento</th>
+                <th colspan="1">#ID Endereço</th>
+                <th>OPÇÕES</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>100</td>
+                <td>50.00</td>
+                <td>50.00</td>
+                <td>50.00</td>
+                <td>50.00</td>
+                <td>50.00</td>
+                <td>
+                  <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button> 
+                  <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+
+                </td>
+              </tr>
+            </tbody>
+          </table>    
+        </div>
+      </form>
         </div>
       </div>
-     
       <br>
-      <div class="grid">
-        <table>
-
-          <thead>
-
-            <tr>
-              <th>#ID</th>
-              <th>NOME</th>
-              <th>CPF</th>
-              <th>E-mail</th>
-              <th>Telefone</th>
-              <th>Data Nascimento</th>
-              <th>#ID Endereço</th>
-              <th>OPÇÕES</th>
-            </tr>
-
-          </thead>
-
-          <tbody>
-
-            <tr>
-
-              <td>Arduino</td>
-              <td>100</td>
-              <td>50.00</td>
-              <td>50.00</td>
-              <td>50.00</td>
-              <td>50.00</td>
-              <td>50.00</td>
-              
-              <td>
-                <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button> 
-                <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
-              </td>
-
-            </tr>
-
-          </tbody>
-        
-        </table>
-      </div>
-
     </div>
-
+           
   </div>
 </template>
 
@@ -97,23 +89,5 @@ export default {
 </script>
 
 <style>
-  .container{
-    position: relative;	
-    width: 100%;
-    float:left;
-  }
-  .campos{
-    position: relative;
-    width: 50%;
-    margin-left: 2%;
-    margin-right: 30%;
-    margin-top: 1%;
-  };
-  .grid{
-    position: relative;
-    width: 50%;
-    margin-left: 50%;
-    margin-right: 50%;
-    margin-top: 5%;
-  }
+
 </style>
---
+++ src/services/config.js
@@ -0,0 +1,5 @@
+import aixos from 'aixos'
+
+export const http = axios.create({
+    baseUrl:
+}) 
